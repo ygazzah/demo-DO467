2622df8478c4c58b56a001c9e863a12cd7fec510
ygazzah <83963609+ygazzah@users.noreply.github.com>
2025-02-18 10:12:27 +0300

    Update inventorya.py

--- inventorya.py
+++ inventorya.py
@@ -5,16 +5,16 @@ import sys
 import json
 
 result = {}
-result['webservers'] = {}
-result['webservers']['hosts'] = []
-result['webservers']['vars'] = {}
+result['dynamicgroup'] = {}
+result['dynamicgroup']['hosts'] = []
+result['dynamicgroup']['vars'] = {}
 
 pipe = Popen(['getent', 'hosts'], stdout=PIPE, universal_newlines=True)
 
 for line in pipe.stdout.readlines():
     s = line.split()
     if s[1].startswith('servera'):
-        result['webservers']['hosts'].append(s[1])
+        result['dynamicgroup']['hosts'].append(s[1])
 
 if len(sys.argv) == 2 and sys.argv[1] == '--list':
     print(json.dumps(result))
